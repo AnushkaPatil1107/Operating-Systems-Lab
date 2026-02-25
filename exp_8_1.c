5d40d46403b1ea9a3c8b440b96964377fd81fd15
AnushkaPatil1107 <anushka.patil.batch2024@sitnagpur.siu.edu.in>
2026-02-25 10:17:19 +0530

    Add files via upload

---
+++ exp_8_1.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+int main()
+{
+int n,i;
+printf("Enter number of processes: ");
+scanf("%d",&n);
+
+int at[20],bt[20],pr[20];
+int ct[20],tat[20],wt[20];
+int rt[20];
+int done[20]={0};
+
+for(i=0;i<n;i++)
+{
+printf("\nProcess P%d\n",i+1);
+
+printf("Arrival Time: ");
+scanf("%d",&at[i]);
+
+printf("Burst Time: ");
+scanf("%d",&bt[i]);
+rt[i]=bt[i];
+
+printf("Priority (Higher number = Higher priority): ");
+scanf("%d",&pr[i]);
+}
+
+int time=0,completed=0;
+
+while(completed<n)
+{
+int max_priority=-1;
+int index=-1;
+
+for(i=0;i<n;i++)
+{
+if(at[i]<=time && done[i]==0)
+{
+if(pr[i]>max_priority)
+{
+max_priority=pr[i];
+index=i;
+}
+}
+}
+
+if(index!=-1)
+{
+rt[index]--;
+time++;
+
+if(rt[index]==0)
+{
+ct[index]=time;
+tat[index]=ct[index]-at[index];
+wt[index]=tat[index]-bt[index];
+done[index]=1;
+completed++;
+}
+}
+else
+{
+time++;
+}
+}
+
+printf("\nP\tAT\tBT\tPR\tCT\tTAT\tWT\n");
+
+for(i=0;i<n;i++)
+{
+printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",i+1,at[i],bt[i],pr[i],ct[i],tat[i],wt[i]);
+}
+
+return 0;
+}
